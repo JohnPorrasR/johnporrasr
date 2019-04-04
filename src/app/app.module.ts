4309ca07cbfa1e311e186313c2af5b1181446594
John Porras R <johnporrasr@gmail.com>
2019-04-04 12:22:32 -0500

    se ha generado la base del proyecto, componentes header, footer y la pagina de inicio. Como tambien se ha carga los assets de la plantilla

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { InicioComponent } from './inicio/inicio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule
